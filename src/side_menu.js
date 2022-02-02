a0537a1dd658fb5351d84ac0c78312fbbed657ef
Avninder Preet Singh <53931646+Avninder99@users.noreply.github.com>
2022-02-03 00:38:00 +0530

    Finished side and bottom bars

--- src/side_menu.js
+++ src/side_menu.js
@@ -2,9 +2,34 @@
 export default function side_menu() {
     return (
         <div className="side-menu">
-
-            <h1>hello</h1>
-
+            <div className="side-option-holder">
+                <a
+                    href="#"
+                    className="side-menu-options"
+                    aria-current="page"
+                    >Mac</a
+                >
+                <a
+                    href="#"
+                    className="side-menu-options"
+                    >iPhone</a
+                >
+                <a
+                    href="#"
+                    className="side-menu-options"
+                    >iPad</a
+                >
+                <a
+                    href="#"
+                    className="side-menu-options-2"
+                    >iWatch</a
+                >
+                <a
+                    href="#"
+                    className="side-menu-options"
+                    >Support</a
+                >
+            </div>
         </div>
     );
 }
