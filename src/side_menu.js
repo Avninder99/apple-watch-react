e9003ed53e82f423f46e316b8f31325fc60c86f0
Avninder Preet Singh <53931646+Avninder99@users.noreply.github.com>
2022-02-03 00:44:50 +0530

    Deploy fix

--- src/side_menu.js
+++ src/side_menu.js
@@ -3,29 +3,24 @@ export default function side_menu() {
     return (
         <div className="side-menu">
             <div className="side-option-holder">
-                <a
-                    href="#"
+                <a href="https://virtual99.netlify.app/"
                     className="side-menu-options"
                     aria-current="page"
                     >Mac</a
                 >
-                <a
-                    href="#"
+                <a href="https://virtual99.netlify.app/"
                     className="side-menu-options"
                     >iPhone</a
                 >
-                <a
-                    href="#"
+                <a href="https://virtual99.netlify.app/"
                     className="side-menu-options"
                     >iPad</a
                 >
-                <a
-                    href="#"
+                <a href="https://virtual99.netlify.app/"
                     className="side-menu-options-2"
                     >iWatch</a
                 >
-                <a
-                    href="#"
+                <a href="https://virtual99.netlify.app/"
                     className="side-menu-options"
                     >Support</a
                 >
