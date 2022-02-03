bd0c6f82df6f0dc6507a06384197b76ce9ac2572
Avninder Preet Singh <53931646+Avninder99@users.noreply.github.com>
2022-02-04 02:06:03 +0530

    Completed navbar

--- src/navbar.js
+++ src/navbar.js
@@ -1,18 +1,25 @@
+import { useState } from "react";
 import apple_icon from "./images/apple_icon.svg";
 import search_icon from "./images/search.svg";
 import bag_icon from "./images/shopping_bag.svg";
 
 // Nav Bar component
-export default function nav_bar() {
-    let width = window.innerWidth;
+export default function Nav_bar() {
+    let width_2 = window.innerWidth;
+    const [searchBarOpen, setSearchBarOpen] = useState(false);
+
+    const searchBarChangeHandler = () => {
+        setSearchBarOpen(!searchBarOpen);
+        console.log("changed");
+    }
+    console.log("ran");
+
     return (
         <nav className="desktop-nav-bar">
             <div className="left-side-nav">
-
                 <div className="main-logo-div pr-28">
                     <img className="main-logo" src = {apple_icon} alt="Apple"/>
                 </div>
-
                 <div className="desktop-link-holder">
                     {/* Desktop menu */}
                     <a
@@ -28,44 +35,65 @@ export default function nav_bar() {
                         className="desktop-nav-links absolute"
                         >iPhone
                     </a>
-                    <a
-                        href="#"
-                        id="link-3"
-                        className="desktop-nav-links absolute"
-                        >iPad
-                    </a>
-                    <a
-                        href="#"
-                        id="link-4"
-                        className="desktop-nav-links-2 absolute"
-                        >iWatch
-                    </a>
-                    <a
-                        href="#"
-                        id="link-5"
-                        className="desktop-nav-links absolute"
-                        >Support
-                    </a>
-                </div>
+                    {
+                        searchBarOpen && (width_2<1024)
+                            ?
+                                ""
+                            :
+                                <>
+                                    <a
+                                        href="#"
+                                        id="link-3"
+                                        className="desktop-nav-links absolute"
+                                        >iPad
+                                    </a>
+                                    <a
+                                        href="#"
+                                        id="link-4"
+                                        className="desktop-nav-links-2 absolute"
+                                        >iWatch
+                                    </a>
 
+                                </>
+                    }
+                    {
+                        searchBarOpen && (width_2<1280)
+                            ?
+                                "" 
+                            :
+                            <a
+                                href="#"
+                                id="link-5"
+                                className="desktop-nav-links absolute"
+                                >Support
+                            </a>
+                    }
+                    
+                </div>
             </div>
 
             {/* Right Side options */}
             <div className="right-side-nav">
-                
-                {/* <div id="search-bar" className="hidden text-left pl-5 pt-2 pr-7 sm:pr-10 md:pr-0 md:w-64 xl:w-72 h-10 bg-white/30 rounded-tl-full rounded-bl-full text-white font-poppins font-normal">
-                    Search here
-                </div> */}
+                {
+                    searchBarOpen
+                        ?
+                            <div id="search-bar" className="">
+                                Search here
+                            </div>
+                        :
+                            ""
+                }
 
                 <button
                     type="button"
-                    id="search_icon_btn"
-                    className="right-icons"
+                    id="search-icon-btn"
+                    className={searchBarOpen ? "right-icons search-active" : "right-icons"}
+                    onClick={searchBarChangeHandler}
                 >
                     <img id="search-icon" className="" src={search_icon} alt="search"/>
                 </button>
                 
-                <div className="ml-3 pl-6 relative border-l-3 border-white/75">
+                <div>
                     <button
                         type="button"
                         className="right-icons"
